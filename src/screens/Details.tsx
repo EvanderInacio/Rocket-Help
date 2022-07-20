9cea48ec973f34dcb0d01050e4971a603b926102
Evander Inacio <evander.20112@hotmail.com>
2022-07-20 10:15:06 -0300

    feat: :fire: Details

---
+++ src/screens/Details.tsx
@@ -0,0 +1,11 @@
+import { VStack } from 'native-base';
+import { Header } from '../components/Header';
+
+export function Details() {
+  return (
+    <VStack flex={1} bg="gray.700">
+      <Header title="solicitação"/>
+
+    </VStack>
+  );
+}
