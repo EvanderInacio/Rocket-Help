83976afc01fd9b1a166ebebb9b0a6e271b79fee0
Evander Inacio <evander.20112@hotmail.com>
2022-07-20 22:05:21 -0300

    Navigation Details :construction:

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,10 +1,21 @@
-import { VStack } from 'native-base';
+import { useRoute } from '@react-navigation/native'
+import { VStack, Text } from 'native-base';
 import { Header } from '../components/Header';
 
+type RouteParams = {
+  orderId: string
+}
+
 export function Details() {
+  const route = useRoute();
+  const { orderId } = route.params as RouteParams;
+
   return (
     <VStack flex={1} bg="gray.700">
       <Header title="solicitação"/>
+      <Text color="white">
+        {orderId}
+      </Text>
 
     </VStack>
   );
