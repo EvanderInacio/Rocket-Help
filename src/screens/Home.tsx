123c7ecd4dcb5ec3c1f6e8ae2a3113adb3eda63c
Evander Inacio <evander.20112@hotmail.com>
2022-07-22 20:39:56 -0300

    Create new date :page_facing_up:

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Alert } from 'react-native'
-import auth  from '@react-native-firebase/auth'
+import auth from '@react-native-firebase/auth'
+import firestore from '@react-native-firebase/firestore'
 import { useNavigation } from '@react-navigation/native'
 import { GenderNonbinary, SignOut } from 'phosphor-react-native'
 import { DesktopTower } from 'phosphor-react-native'
@@ -22,35 +23,59 @@ import { Button } from '../components/Button'
 import { Order, OrderProps } from '../components/Order'
 
 export function Home() {
+  const [loading, setLoading] = useState(true)
   const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>(
     'open'
   )
-  const [orders, setOrders] = useState<OrderProps[]>([ 
-   {
-    id: '123',
-    patrimony: '123456',
-    when: '20/07/2022 às 21:44',
-    status: 'open'
-   }
+  const [orders, setOrders] = useState<OrderProps[]>([
+    {
+      id: '123',
+      patrimony: '123456',
+      when: '20/07/2022 às 21:44',
+      status: 'open'
+    }
   ])
 
   const navigation = useNavigation()
   const { colors } = useTheme()
 
-  function handleNewOrder(){
-    navigation.navigate('new');
+  function handleNewOrder() {
+    navigation.navigate('new')
   }
 
-  function handleOpenDetails( orderId: string){
+  function handleOpenDetails(orderId: string) {
     navigation.navigate('details', { orderId })
   }
 
   function handleLogout() {
-    auth().signOut().catch(error => {
-      console.log(error);
-      return Alert.alert('Sair' , 'Não foi possível sair.')
-    })
+    auth()
+      .signOut()
+      .catch(error => {
+        console.log(error)
+        return Alert.alert('Sair', 'Não foi possível sair.')
+      })
   }
+
+  useEffect(() => {
+    setLoading(true)
+
+    const subscriber = firestore()
+      .collection('orders')
+      .where('status', '==', statusSelected)
+      .onSnapshot(snapshot => {
+        const data = snapshot.docs.map(doc => {
+          const { patrimony, description, status, created_at } = doc.data()
+
+          return {
+            id: doc.id,
+            patrimony,
+            description,
+            status,
+            when: 
+          }
+        })
+      })
+  }, [])
 
   return (
     <VStack flex={1} pb={6} bg="gray.700">
@@ -65,8 +90,8 @@ export function Home() {
       >
         <Logo />
 
-        <IconButton 
-          icon={<SignOut size={26} color={colors.gray[300]} />} 
+        <IconButton
+          icon={<SignOut size={26} color={colors.gray[300]} />}
           onPress={handleLogout}
         />
       </HStack>
@@ -79,13 +104,9 @@ export function Home() {
           justifyContent="space-between"
           alignContent="center"
         >
-          <Heading color="gray.100">
-            Solicitações
-          </Heading>
+          <Heading color="gray.100">Solicitações</Heading>
 
-          <Text color="gray.200">
-            {orders.length}
-          </Text>
+          <Text color="gray.200">{orders.length}</Text>
         </HStack>
 
         <HStack space={3} mb={8}>
@@ -107,25 +128,25 @@ export function Home() {
         <FlatList
           data={orders}
           keyExtractor={item => item.id}
-          renderItem={({ item }) => <Order data={item} 
-          onPress={() => handleOpenDetails(item.id)} 
-        />} 
+          renderItem={({ item }) => (
+            <Order data={item} onPress={() => handleOpenDetails(item.id)} />
+          )}
           showsVerticalScrollIndicator={false}
           contentContainerStyle={{ paddingBottom: 100 }}
           ListEmptyComponent={() => (
             <Center>
-              <DesktopTower color={ colors.gray[300] } size={70}/>
+              <DesktopTower color={colors.gray[300]} size={70} />
               <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
                 Você ainda não possui {'\n'}
-                solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
-              </Text> 
+                solicitações{' '}
+                {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
+              </Text>
             </Center>
           )}
         />
 
-        <Button title="Nova solicitação"  onPress={handleNewOrder}/>
+        <Button title="Nova solicitação" onPress={handleNewOrder} />
       </VStack>
     </VStack>
   )
 }
-
