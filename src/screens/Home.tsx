a5be010c9753c908894abf8e7eaf0ec72b5f3907
Evander Inacio <evander.20112@hotmail.com>
2022-07-22 18:18:33 -0300

    Logout :seedling:

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { Alert } from 'react-native'
+import auth  from '@react-native-firebase/auth'
 import { useNavigation } from '@react-navigation/native'
 import { GenderNonbinary, SignOut } from 'phosphor-react-native'
 import { DesktopTower } from 'phosphor-react-native'
@@ -43,6 +45,13 @@ export function Home() {
     navigation.navigate('details', { orderId })
   }
 
+  function handleLogout() {
+    auth().signOut().catch(error => {
+      console.log(error);
+      return Alert.alert('Sair' , 'Não foi possível sair.')
+    })
+  }
+
   return (
     <VStack flex={1} pb={6} bg="gray.700">
       <HStack
@@ -56,7 +65,10 @@ export function Home() {
       >
         <Logo />
 
-        <IconButton icon={<SignOut size={26} color={colors.gray[300]} />} />
+        <IconButton 
+          icon={<SignOut size={26} color={colors.gray[300]} />} 
+          onPress={handleLogout}
+        />
       </HStack>
 
       <VStack flex={1} px={6}>
